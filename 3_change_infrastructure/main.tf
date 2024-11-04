04ae11e884382aaa3849d525f0a3f8b99511f161
Himal Pandey <pandu.himal@gmail.com>
2024-11-05 06:53:46 +1100

    3 - Initial infrastructure

---
+++ 3_change_infrastructure/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+# configure aws provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# create ec2 instance
+resource "aws_instance" "web_server" {
+  ami           = "ami-06b21ccaeff8cd686"
+  instance_type = "t2.micro" # free tier
+
+  tags = {
+    Name = "ExampleWebServerInstance"
+  }
+}
